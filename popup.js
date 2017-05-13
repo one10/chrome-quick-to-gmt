653641d7ab413f425040d9074afa958d6b80533f
one10 <ivan.balepin@gmail.com>
2017-05-13 11:56:14 -0700

    fixing bugs, making sure the conversion actually works

--- popup.js
+++ popup.js
@@ -6,14 +6,40 @@ function loadScript(scriptName) {
     document.head.appendChild(scriptEl);
 }
 
+function formatDate(inp) {
+    // Wed Dec 30 2015 00:08:00 GMT
+    // 00:08:00 GMT, Wed Dec 30 2015
+    return inp.toString('HH:mm:ss ddd MMM dd yyyy');
+}
+
 function convertDate(e) {
     loadScript('lib/date.js');
 
     dStr = document.querySelector('#datebox').value;
-    d = Date.parse('today');
+    d = Date.parse(dStr);
 
-    document.querySelector('#out').innerHTML = '<span style="color: green; ">PST > GMT:</span> ' + d
-        + '<br><span style="color: green; ">GMT > PST</span>: ' + d;
+    if (!d) {
+        document.querySelector('#out').innerHTML = '<span style="color: green; ">Orig date:</span> ' + dStr + '<hr><br><br>';
+        return;
+    }
+
+    // first, treat this date as Pacific. Then convert to GMT
+    offset = 8;
+    if (Date.today().isDaylightSavingTime()) {
+        offset = 7;
+    }
+    localTz = 'PST';
+    if (Date.today().isDaylightSavingTime()) {
+        localTz = 'PDT';
+    }
+    dG = formatDate(d.addHours(offset)) + ' GMT';
+    dP = formatDate(d.addHours(-offset * 2)) + ' ' + localTz;
+
+
+    document.querySelector('#out').innerHTML =
+        '<span style="color: green; ">Orig date:</span> ' + formatDate(d.addHours(offset))
+        + '<hr><span style="color: green; ">' + localTz + " > GMT:</span> " + dG
+        + '<br><span style="color: green; ">GMT > ' + localTz + "</span>: " + dP;
 }
 
 // action for the extension popup form
