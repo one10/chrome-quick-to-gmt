c2aa0b4bc7341fbe487fb0e5a35fbc58bbbf5c22
one10 <ivan.balepin@gmail.com>
2017-05-13 12:14:24 -0700

    linted the popup.js file

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 // https://github.com/one10/chrome-quick-to-gmt
 
 function loadScript(scriptName) {
-    var scriptEl = document.createElement('script');
+    const scriptEl = document.createElement('script');
     scriptEl.src = chrome.extension.getURL(scriptName);
     document.head.appendChild(scriptEl);
 }
@@ -15,31 +15,25 @@ function formatDate(inp) {
 function convertDate(e) {
     loadScript('lib/date.js');
 
-    dStr = document.querySelector('#datebox').value;
-    d = Date.parse(dStr);
+    const dStr = document.querySelector('#datebox').value;
+    const d = Date.parse(dStr);
 
     if (!d) {
-        document.querySelector('#out').innerHTML = '<span style="color: green; ">Orig date:</span> ' + dStr + '<hr><br><br>';
+        document.querySelector('#out').innerHTML = `<span style="color: green; ">Orig date:</span> ${dStr} <hr><br><br>`;
         return;
     }
 
     // first, treat this date as Pacific. Then convert to GMT
-    offset = 8;
-    if (Date.today().isDaylightSavingTime()) {
-        offset = 7;
-    }
-    localTz = 'PST';
-    if (Date.today().isDaylightSavingTime()) {
-        localTz = 'PDT';
-    }
-    dG = formatDate(d.addHours(offset)) + ' GMT';
-    dP = formatDate(d.addHours(-offset * 2)) + ' ' + localTz;
+    const offset = (Date.today().isDaylightSavingTime()) ? 7 : 8;
+    const localTz = (Date.today().isDaylightSavingTime()) ? 'PDT' : 'PST';
 
+    const dG = `${formatDate(d.addHours(offset))}  GMT`;
+    const dP = `${formatDate(d.addHours(-offset * 2))} ${localTz}`;
 
     document.querySelector('#out').innerHTML =
-        '<span style="color: green; ">Orig date:</span> ' + formatDate(d.addHours(offset))
-        + '<hr><span style="color: green; ">' + localTz + " > GMT:</span> " + dG
-        + '<br><span style="color: green; ">GMT > ' + localTz + "</span>: " + dP;
+        `<span style="color: green; ">Orig date:</span> ${formatDate(d.addHours(offset))} <hr>
+        <span style="color: green; "> ${localTz} > GMT:</span> ${dG} <br>
+        <span style="color: green; ">GMT > ${localTz}</span>: ${dP}`;
 }
 
 // action for the extension popup form
