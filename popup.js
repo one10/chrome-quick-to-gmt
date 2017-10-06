aa0a3f94a187ee608288bc821632654643a7bb6f
one10 <ivan.balepin@gmail.com>
2017-10-06 02:45:24 -0700

    refactored for easier testing of the date conversion logic, added one more test

--- popup.js
+++ popup.js
@@ -6,43 +6,55 @@ function loadScript(scriptName) {
     document.head.appendChild(scriptEl);
 }
 
+// given a date, e.g.: Date.parse('Wed Dec 30 2015 00:08:00 GMT')
+// output a string, e.g.: '00:08:00 Wed Dec 30 2015'
 function formatDate(inp) {
-    // Wed Dec 30 2015 00:08:00 GMT
-    // 00:08:00 GMT, Wed Dec 30 2015
     return inp.toString('HH:mm:ss ddd MMM dd yyyy');
 }
 
-function convertDate(e) {
+// given a date string in any parseable format
+// return an array: [originalDate, gmtDate, localDate]
+function convertDate(dateStr) {
+    const inpDate = Date.parse(dateStr);
+    if (!inpDate) {
+        return null;
+    }
+    // first, treat this date as Pacific. Then convert to GMT
+    const offset = (Date.today().isDaylightSavingTime()) ? 7 : 8;
+
+    const originalDate = formatDate(new Date(inpDate.getTime()));
+    const gmtDate = formatDate(new Date(inpDate.getTime()).addHours(offset));
+    const localDate = formatDate(new Date(inpDate.getTime()).addHours(-offset));
+
+    return [originalDate, gmtDate, localDate];
+}
+
+function prepConvertDate() {
     loadScript('lib/date.js');
 
-    const dStr = document.querySelector('#datebox').value;
-    const d = Date.parse(dStr);
+    const dateStr = document.querySelector('#datebox').value;
+    const conversionResults = convertDate(dateStr);
 
-    if (!d) {
-        document.querySelector('#out').innerHTML = `<span style="color: green;">Orig date:</span>${dStr}<hr><br><br>`;
+    if (!conversionResults) {
+        document.querySelector('#out').innerHTML = `<span style="color: green;">Orig date:</span>${dateStr}<hr><br><br>`;
         return;
     }
 
-    // first, treat this date as Pacific. Then convert to GMT
-    const offset = (Date.today().isDaylightSavingTime()) ? 7 : 8;
     const localTz = (Date.today().isDaylightSavingTime()) ? 'PDT' : 'PST';
-
-    const dG = `${formatDate(d.addHours(offset))}  GMT`;
-    const dP = `${formatDate(d.addHours(-offset * 2))} ${localTz}`;
-
     document.querySelector('#out').innerHTML =
-        `<span style="color: green; ">Orig date:</span> ${formatDate(d.addHours(offset))} <hr>
-        <span style="color: green; "> ${localTz} > GMT:</span> ${dG} <br>
-        <span style="color: green; ">GMT > ${localTz}</span>: ${dP}`;
+        `<span style="color: green; ">Orig date:</span> ${conversionResults[0]} <hr>
+        <span style="color: green; "> ${localTz} > GMT:</span> ${conversionResults[1]} GMT <br>
+        <span style="color: green; ">GMT > ${localTz}</span>: ${conversionResults[2]} ${localTz}`;
 }
 
 // action for the extension popup form
 if (typeof document !== 'undefined') {
     document.addEventListener('DOMContentLoaded', function () {
-        document.getElementById('datebox').addEventListener('input', convertDate);
+        document.getElementById('datebox').addEventListener('input', prepConvertDate);
     });
 }
 
 module.exports = {
-    formatDate
+    formatDate,
+    convertDate
 };
--- test/test_popup.js
+++ test/test_popup.js
@@ -3,14 +3,32 @@
 /* global describe, it */
 
 const assert = require('assert');
+const moment = require('moment');
 const popup = require('../popup.js');
 require('../lib/date.js');
 
 describe('formatDate(inp)', function () {
-    it('should retugrn string in format "HH:mm:ss ddd MMM dd yyyy" given a proper date', function () {
+    it('should return string in format "HH:mm:ss ddd MMM DD YYYY" given a proper date', function () {
         const testDate1 = Date.parse('Wed Dec 30 2015 00:08:00 GMT');
         // by defaut, formatDate will return a local date, so need to adjust it to make sure we get GMT back to compare
-        const offset = testDate1.getTimezoneOffset() / 60;
-        assert.equal(popup.formatDate(testDate1.addHours(offset)), '00:08:00 Wed Dec 30 2015');
+        const offset = testDate1.getTimezoneOffset();
+        assert.equal(popup.formatDate(testDate1.addMinutes(offset)), '00:08:00 Wed Dec 30 2015');
+    });
+});
+
+describe('formatDate(inp)', function () {
+    it('should use format "HH:mm:ss ddd MMM DD YYYY" as its output', function () {
+        const testDate1 = Date.parse('Wed Dec 30 2015 00:08:00 GMT');
+        const offset = testDate1.getTimezoneOffset();
+        assert.ok(moment(popup.formatDate(testDate1.addMinutes(offset)), 'HH:mm:ss ddd MMM DD YYYY', true).isValid());
+    });
+});
+
+describe('convertDate(dateStr)', function () {
+    it('should return properly converted values for input "Mar-30 4:21 PM"', function () {
+        const res1 = popup.convertDate('Mar-30 4:21 PM');
+        assert.equal(res1[0], '16:21:00 Thu Mar 30 2017');
+        assert.equal(res1[1], '23:21:00 Thu Mar 30 2017');
+        assert.equal(res1[2], '09:21:00 Thu Mar 30 2017');
     });
 });
